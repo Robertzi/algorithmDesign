f539069023ed0a0677e39801cb5997bc7d14f237
Robertzi <549899794@qq.com>
2018-06-05 09:23:44 +0800

    inputfile

---
+++ utiliy.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+#include "utiliy.h"
+
+int * InputFromFile(const char *s, int number)
+{
+	std::string filename = s;
+	std::fstream inputfs;
+	inputfs.open(filename);
+	if (!inputfs.is_open())
+	{
+		inputfs.clear();
+		inputfs.open(filename, std::ios::out); // 创建文件。
+		inputfs.close();
+		inputfs.open(filename);
+		return NULL;
+	}
+	char buffer[256];
+	inputfs.getline(buffer, 256);
+	//std::cout << buffer << std::endl;
+	char *p = strtok(buffer, "\t");
+	int i = 0, *arr = new int[number];
+	while (p&&i<number)
+	{
+		arr[i] = atoi(p);
+		//std::cout << p << ' ';
+		p = strtok(NULL, "\t");
+		i++;
+	}
+	return arr;
+}
+
+int randRange(int min, int max)
+{
+	return (int)((max - min + 1)*((double)rand() / RAND_MAX) + min);
+}
+
+int * Randoms(int min, int max, int number)
+{
+	int *p = new int[number];
+	for (int i = 0; i < number; i++)
+	{
+		p[i] = randRange(min,max);
+	}
+	return p;
+}
+
+void printArray(int *p, int col, int row)
+{
+	for (int i = 0; i < col*row; i++)
+	{
+		std::cout << p[i] << ' ';
+		if (i%col == col - 1) std::cout << std::endl;
+	}
+}
---
+++ utiliy.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int randRange(int min = 0, int max = 99);
+int * Randoms(int min = 0, int max = 99, int number = 10);
+int * InputFromFile(const char *s ,int number);
+
+void printArray(int *p ,int col, int row = 1);
